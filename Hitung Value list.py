192a88cc0eb6daa37247fc98f8f42381524013b9
Drianyusupp <91178655+Drianyusupp@users.noreply.github.com>
2021-11-24 09:56:30 +0700

    Add files via upload

---
+++ Hitung Value list.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 24 09:34:44 2021
+
+@author: User
+"""
+
+deret = [10, 7, 8,]
+print("List : ",deret)
+print("Hasil Kali seluruh value pada list :",10*7*8)
+
---
+++ Konversi List ke Tuple.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 24 08:54:56 2021
+
+@author: User
+"""
+makhluk_darat = [Singa, Harimau, Kerbau, Zebra, Kambing, Kuda]
+print("List : ",makhluk_darat)
+print("Hasil Reverse List menjadi Tuple : ")
+print(tuple(makhluk_darat))
